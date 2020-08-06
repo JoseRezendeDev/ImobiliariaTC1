dc7a54bb8c3fe5bc744e5b5ca41d94527e51db4d
Felipe <felipe@superempresa.com.br>
2020-08-06 11:00:49 -0300

    Criado enums e atributos do cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,68 @@
+import java.util.Date;
+import java.util.List;
+
 public class Cliente {
+    private String cpf;
+    private String nome;
+    private Date dataNascimento;
+    private Enum<Sexo> sexo;
+    private Double salario;
+    private List<String> emails;
+    private List<String> telefones;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Enum<Sexo> getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Enum<Sexo> sexo) {
+        this.sexo = sexo;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    public List<String> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(List<String> emails) {
+        this.emails = emails;
+    }
+
+    public List<String> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<String> telefones) {
+        this.telefones = telefones;
+    }
 }
