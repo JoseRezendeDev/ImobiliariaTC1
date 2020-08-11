b742cd267914c799ef461a27c534f6fb36f5d9a4
Felipe <felipe@superempresa.com.br>
2020-08-11 10:27:53 -0300

    Implementando classe DAO para o imovel

--- src/dao/ImovelDAO.java
+++ src/dao/ImovelDAO.java
@@ -1,4 +1,79 @@
 package dao;
 
+import model.Imovel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ImovelDAO {
+    private List<Imovel> imovelList;
+
+    public ImovelDAO() {
+        imovelList = new ArrayList<>();
+    }
+
+    public Boolean incluirImovel(Imovel imovelNovo){
+        for(Imovel imovel : imovelList){
+            if(imovel.getCodigo().equals(imovelNovo.getCodigo())){
+                // Já existe um imovel com esse código
+                return false; // interrompe o metodo
+            }
+        }
+
+        imovelList.add(imovelNovo);
+        return true;
+    }
+
+    public Boolean alterarImovel(String codigo, Imovel imovelAtualizado){
+        for(Imovel imovel : imovelList){
+            if(codigo.equals(imovel.getCodigo())){
+                imovel.setCodigo(imovelAtualizado.getCodigo());
+
+                imovel.setCep(imovelAtualizado.getCep());
+                imovel.setRua(imovelAtualizado.getRua());
+                imovel.setNumero(imovelAtualizado.getNumero());
+                imovel.setBairro(imovelAtualizado.getBairro());
+                imovel.setEstado(imovelAtualizado.getEstado());
+                imovel.setCidade(imovelAtualizado.getCidade());
+                imovel.setPais(imovelAtualizado.getPais());
+
+                imovel.setQuantidadeGaragens(imovelAtualizado.getQuantidadeGaragens());
+                imovel.setQuantidadeQuartos(imovelAtualizado.getQuantidadeQuartos());
+                imovel.setQuantidadeSeguranca(imovelAtualizado.getQuantidadeSeguranca());
+                imovel.setQuantidadeSuites(imovelAtualizado.getQuantidadeSuites());
+
+                imovel.setTipo(imovelAtualizado.getTipo());
+                imovel.setDescricao(imovelAtualizado.getDescricao());
+                imovel.setValorAluguel(imovelAtualizado.getValorAluguel());
+                imovel.setValorCondominio(imovelAtualizado.getValorCondominio());
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Boolean excluirImovel(Imovel imovelExcluir){
+        for(Imovel imovel : imovelList){
+            if(imovel.getCodigo().equals(imovelExcluir.getCodigo())){
+                imovelList.remove(imovel);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Imovel getImovel(String codigo){
+        for(Imovel imovel : imovelList){
+            if(imovel.getCodigo().equals(codigo)){
+                return imovel;
+            }
+        }
+        return null;
+    }
+
+    public List<Imovel> getImoveis(){
+        return imovelList;
+    }
 }
